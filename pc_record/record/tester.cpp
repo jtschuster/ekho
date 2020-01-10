c74a210e09b9285d425680893acb786a8ae0882f
jtschuster <jacksontschuster@gmail.com>
2020-01-10 15:00:08 -0600

    removed unnecessary files

--- pc_record/record/tester.cpp
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-int main() { 
-    printf("%d\n", sizeof(double)); // some compilers print 8
-    printf("%d\n", sizeof(long double)); // some compilers print 16
-    return 0;
-}
